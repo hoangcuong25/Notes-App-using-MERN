04f25bccda3d0c92e862f0c9179bd2fd76449584
hoang cuong <vancuong25112003@gmail.com>
2024-10-09 20:16:25 +0700

    update frontend

---
+++ frontend/vite-project/src/App.jsx
@@ -0,0 +1,25 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from "./pages/Home/Home"
+import Login from "./pages/Login/Login"
+import Signup from "./pages/Signup/Signup"
+
+const routes = (
+  <BrowserRouter>
+    <Routes>
+      <Route path="/dashboard" exact element={<Home />} />
+      <Route path="/login" exact element={<Login />} />
+      <Route path="/signup" exact element={<Signup />} />
+    </Routes>
+  </BrowserRouter>
+)
+
+function App() {
+
+  return (
+    <>
+      {routes}
+    </>
+  )
+}
+
+export default App
