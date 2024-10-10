d90aadf08ca57764b218e8fb29c74da52b188d9d
hoang cuong <vancuong25112003@gmail.com>
2024-10-11 05:28:54 +0700

    add note

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,7 @@ const mongoose = require("mongoose")
 mongoose.connect(config.connectionString)
 
 const User = require("./models/user.model")
+const Note = require("./models/note.model")
 
 const express = require("express")
 const cors = require("cors")
@@ -73,6 +74,7 @@ app.post("/create-account", async (req, res) => {
     })
 })
 
+// login
 app.post("/login", async (req, res) => {
     const { email, password } = req.body
 
@@ -94,6 +96,8 @@ app.post("/login", async (req, res) => {
         const user = { user: userInfo }
         const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECERT, { expiresIn: "36000m" })
 
+        // const accessToken = jwt.sign({ userInfo }, process.env.ACCESS_TOKEN_SECERT, { expiresIn: "36000m" })
+
         return res.json({
             error: false,
             message: "Login Successful",
@@ -104,6 +108,41 @@ app.post("/login", async (req, res) => {
         return res.status(400).json({
             error: true,
             message: "Invalid Credentials"
+        })
+    }
+})
+
+// add note
+app.post("/add-note", authenticateToken, async (req, res) => {
+    const { title, content, tags } = req.body
+    const { user } = req.user
+
+    if (!title) {
+        return res.status(400).json({ error: true, message: "Title is required" })
+    }
+
+    if (!content) {
+        return res.status(400).json({ error: true, message: "Content is required" })
+    }
+
+    try {
+        const note = new Note({
+            title,
+            content,
+            tags: tags || [],
+            userId: user._id,
+        })
+
+        await note.save();
+        return res.json({
+            error: false,
+            note,
+            message: "Note added successfully"
+        })
+    } catch (error) {
+        return res.status(500).json({
+            error: true,
+            message: "Internal Server Error"
         })
     }
 })
