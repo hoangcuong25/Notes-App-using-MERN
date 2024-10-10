e669cb2f4f81ce3abf90dd6355f8544f134fc6c3
hoang cuong <vancuong25112003@gmail.com>
2024-10-11 04:00:53 +0700

    Login api

--- backend/index.js
+++ backend/index.js
@@ -50,7 +50,7 @@ app.post("/create-account", async (req, res) => {
 
     if (isUser) {
         return res.json({
-            error : true,
+            error: true,
             message: "User already exist"
         })
     }
@@ -63,7 +63,7 @@ app.post("/create-account", async (req, res) => {
 
     await user.save();
 
-    const accessToken = jwt.sign({user}, process.env.ACCESS_TOKEN_SECERT, {expiresIn: "36000m",})
+    const accessToken = jwt.sign({ user }, process.env.ACCESS_TOKEN_SECERT, { expiresIn: "36000m", })
 
     return res.json({
         error: false,
@@ -71,6 +71,41 @@ app.post("/create-account", async (req, res) => {
         accessToken,
         message: "Registration Successful",
     })
+})
+
+app.post("/login", async (req, res) => {
+    const { email, password } = req.body
+
+    if (!email) {
+        return res.status(400).json({ message: "Email is required" })
+    }
+
+    if (!password) {
+        return res.status(400).json({ message: "Password is required" })
+    }
+
+    const userInfo = await User.findOne({ email: email })
+
+    if (!userInfo) {
+        return res.status(400).json({ message: "User not found" })
+    }
+
+    if (userInfo.email == email && userInfo.password == password) {
+        const user = { user: userInfo }
+        const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECERT, { expiresIn: "36000m" })
+
+        return res.json({
+            error: false,
+            message: "Login Successful",
+            email,
+            accessToken
+        })
+    } else {
+        return res.status(400).json({
+            error: true,
+            message: "Invalid Credentials"
+        })
+    }
 })
 
 app.listen(8000)
